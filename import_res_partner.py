4a2898e51197532f56162fdde2216c7ea9be223f
poehlmann <bruno.poehlmann@hotmail.com>
2017-08-30 16:54:53 -0400

    importa productos con marca,atributos,categoria a primer nivel a segundo nivel.

--- import_res_partner.py
+++ import_res_partner.py
@@ -1,14 +1,12 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#********************************************************#
+# ********************************************************#
 # Importación Productos#
-#********************************************************#
+# ********************************************************#
 
-import os
 import csv
 import xmlrpclib
-import re
 
 # HOST='192.168.20.87'
 # PORT=80
@@ -17,43 +15,51 @@ import re
 # PASS='admin'
 # url ='http://%s:%d/xmlrpc/' % (HOST,PORT)
 
-HOST='localhost'
-PORT=8010
-DB='femenina'
-USER='admin'
-PASS='Redesk2017'
-url ='http://%s:%d/xmlrpc/' % (HOST,PORT)
+HOST = 'localhost'
+PORT = 8010
+DB = 'femenina'
+USER = 'admin'
+PASS = 'Redesk2017'
+url = 'http://%s:%d/xmlrpc/' % (HOST, PORT)
 
-common_proxy = xmlrpclib.ServerProxy(url+'common')
-object_proxy = xmlrpclib.ServerProxy(url+'object')
-uid = common_proxy.login(DB,USER,PASS)
+common_proxy = xmlrpclib.ServerProxy(url + 'common')
+object_proxy = xmlrpclib.ServerProxy(url + 'object')
+uid = common_proxy.login(DB, USER, PASS)
 
-def _verify(data,table,type,id):
+
+def _verify(data, table, types, ids):
     # category_model = object_proxy.get_model("product.public.category")
     # ids = category_model.search([("name", "=", category)])
-    args = [('name', '=', str(data))] #query clause
-    ids = object_proxy.execute(DB, uid, PASS, table, 'search', args)
-    if type=='attribute':
-        if ids:
-            return ids
+    args = [('name', '=', str(data))]  # query clause
+    id = object_proxy.execute(DB, uid, PASS, table, 'search', args)
+    if types == 'attribute':
+        if id:
+            return id
         else:
-            vals_attribute = {'name':data,'type':'select','create_variant':True}
-            val= object_proxy.execute(DB,uid,PASS,table,'create',vals_attribute)
+            vals_attribute = {'name': data, 'type': 'select', 'create_variant': True}
+            val = object_proxy.execute(DB, uid, PASS, table, 'create', vals_attribute)
             return [val]
-    elif type=='attribute_val':
-        if ids:
-            return ids
+    elif types == 'attribute_val':
+        if id:
+            return id
         else:
-            vals_attribute_val={'name':data,'attribute_id':id }
-            val = object_proxy.execute(DB,uid,PASS,'product.attribute.value','create',vals_attribute_val)
+            vals_attribute_val = {'name': data, 'attribute_id': ids}
+            val = object_proxy.execute(DB, uid, PASS, table, 'create', vals_attribute_val)
             return [val]
+    elif types == 'search_category':
+        args = [('id', '=', ids)]
+        id = object_proxy.execute(DB, uid, PASS, table, 'search', args)
+        vals_product_subcategory = {'parent_id': id[0], 'name': data}
+        val = object_proxy.execute(DB, uid, PASS, table, 'create', vals_product_subcategory)
+        return [val]
     else:
-        if ids:
-            return ids
+        if id:
+            return id
         else:
-            vals_product_category = {'name':data,'sequence':0}
-            val= object_proxy.execute(DB,uid,PASS,table,'create',vals_product_category)
+            vals_product_category = {'name': data}
+            val = object_proxy.execute(DB, uid, PASS, table, 'create', vals_product_category)
             return [val]
+
 
 def _create(estado):
     global id_product_template, id_product_category
@@ -63,34 +69,48 @@ def _create(estado):
         cont = 1
 
         for field in archive:
-            id_category = _verify(field['categoria'],'product.public.category','',0)
-            id_brand = _verify(field['Marca'],'product.brand','',0)
+            id_category = _verify(field['categoria'], 'product.public.category', '', 0)
 
-            vals_product_template = {'product_brand_id':id_brand[0],'public_categ_ids' :[(6,0,id_category)],
-                                     'name':field['Modelo'],'standar_price':field['precio'],'list_price':field['precio'],
-                                     'mes_type':'fixed','uom_id':1,'iom_po_id':1,'type':'product',
-                                     'procure_method':'make_to_stock','cost_method':'standard','supply_method':'buy',
-                                     'sale_ok':True,'website_published':1,'x_idroghur':field['id']}
-            product = object_proxy.execute(DB,uid,PASS,'product.template','search',[('x_idroghur','=',field['id'])])
+            id_subcategory = _verify(field['subcategoria'], 'product.public.category', 'search_category', id_category)
+
+            id_brand = _verify(field['Marca'], 'product.brand', '', 0)
+
+            if (id_subcategory != ''):
+                id_category = id_subcategory
+
+            vals_product_template = {'product_brand_id': id_brand[0], 'public_categ_ids': [(6, 0, id_category)],
+                                     'name': field['Modelo'], 'standar_price': field['precio'],
+                                     'list_price': field['precio'],
+                                     'mes_type': 'fixed', 'uom_id': 1, 'iom_po_id': 1, 'type': 'product',
+                                     'procure_method': 'make_to_stock', 'cost_method': 'standard',
+                                     'supply_method': 'buy',
+                                     'sale_ok': True, 'website_published': 1, 'x_idroghur': field['id']}
+
+            product = object_proxy.execute(DB, uid, PASS, 'product.template', 'search',
+                                           [('x_idroghur', '=', field['id'])])
 
             if product:
                 product_id = product and product[0]
-                do_write = object_proxy.execute(DB,uid,PASS,'product.template', 'write', product_id, vals_product_template)
+                do_write = object_proxy.execute(DB, uid, PASS, 'product.template', 'write', product_id,
+                                                vals_product_template)
             else:
-                id_product_template = object_proxy.execute(DB,uid,PASS,'product.template','create',vals_product_template)
-                id_attribute = _verify('Potencia (W)','product.attribute','attribute',0)
-                id_attribute_val = _verify(field['Potencia (W)'],'product.attribute.value','attribute_val',id_attribute[0])
+                id_product_template = object_proxy.execute(DB, uid, PASS, 'product.template', 'create',
+                                                           vals_product_template)
+                id_attribute = _verify('Potencia (W)', 'product.attribute', 'attribute', 0)
+                id_attribute_val = _verify(field['Potencia (W)'], 'product.attribute.value', 'attribute_val',
+                                           id_attribute[0])
 
                 product_attribute_line = object_proxy.execute(DB, uid, PASS,
-                                                               'product.attribute.line','create',
-                                                               { 'product_tmpl_id' : id_product_template,
-                                                                 'attribute_id':id_attribute[0]
-                                                                 ,'value_ids': [[6, 0,id_attribute_val]]
-                                                                })
+                                                              'product.attribute.line', 'create',
+                                                              {'product_tmpl_id': id_product_template,
+                                                               'attribute_id': id_attribute[0],
+                                                               'value_ids': [[6, 0, id_attribute_val]]
+                                                               })
                 if id_product_template:
-                    cont = cont + 1
+                    cont += 1
                 else:
                     print "Error"
+
 
 # def _update(estado):
 #     if estado is True:
@@ -132,4 +152,6 @@ def __main__():
     # _update(True)
     # _update_mass(True)
     print 'Ha finalizado la carga tabla'
+
+
 __main__()
